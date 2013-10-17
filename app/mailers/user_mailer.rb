082247d262010eb4d75de95d28fd83f6cac163fd
TheNinJedi <TheNinJedi@gmail.com>
2013-10-16 18:46:56 -0600

    Creates email layout. Then applies it to the welcome email and the password reset email.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,8 @@
 class UserMailer < ActionMailer::Base
   default from: "noreply@TNJ.com"
 
+  layout 'email'
+  
   def welcome(user)
     @user = user
     mail(to: @user.email, subject: "Thank You for Signing Up.")
