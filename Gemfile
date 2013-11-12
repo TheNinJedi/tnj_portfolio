97e3b7309a061513c8df133f90a52aa639e075ea
TheNinJedi <TheNinJedi@gmail.com>
2013-11-11 18:59:54 -0700

    Integrates AWS functionality. S3 and Cloudfront

--- Gemfile
+++ Gemfile
@@ -52,6 +52,7 @@ group :assets do
   gem 'coffee-rails', '~> 3.2.1'
   gem "bootstrap-sass", github: "thomas-mcdonald/bootstrap-sass", branch: '3'
   gem 'uglifier', '>= 1.0.3'
+  gem 'asset_sync'
 
   # See https://github.com/sstephenson/execjs#readme for more supported runtimes
   # gem 'therubyracer', :platforms => :ruby
--- config/application.rb
+++ config/application.rb
@@ -60,5 +60,16 @@ module TnjPortfolio
     config.assets.version = '1.0'
     
     config.assets.initialize_on_precompile = false
+
+    config.paperclip_defaults = {
+        :default_url => "#{Rails.root}/assets/:attachment/default/:style.png",
+        :storage => :s3, 
+        :s3_credentials => {
+             :access_key_id =>  ENV['S3_KEY'], 
+             :secret_access_key =>  ENV['S3_SECRET'] 
+             }, 
+             :s3_protocol => 'https', 
+             :bucket =>  ENV['S3_BUCKET'], 
+             :path => "/:attachment/:id/:style.:extension"
   end
 end
